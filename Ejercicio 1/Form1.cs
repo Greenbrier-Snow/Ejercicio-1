bc31ff859ed1f0bb0a83deb75690560dc001a794
DELL_19 <DELL_19@DESKTOP-1FAS9HD>
2023-05-16 12:39:17 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 1/Form1.cs
@@ -0,0 +1,41 @@
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1, n2, suma;
+            n1 = int.Parse(textBox1.Text);
+            n2 = int.Parse(textBox2.Text);
+            suma = n1 + n2;
+            textBox3.Text = suma.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+    }
+}
